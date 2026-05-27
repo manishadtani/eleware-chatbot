4acce86eb4db6195898b6bf7fd29c972fa7c0683
Prathammeena1 <yashmeena274@gmail.com>
2026-05-27 12:09:35 +0530

    update floating button text

--- app/components/AiChatWrapper.tsx
+++ app/components/AiChatWrapper.tsx
@@ -224,7 +224,7 @@ export default function AiChatWrapper() {
         <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
         </svg>
-        <span>Need help? Chat with us</span>
+        <span>Got a question? Let&apos;s talk 💬</span>
 
         {/* Pulse ring */}
         <span
